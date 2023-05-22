deedb0f84010b963e417d6b623eacd4b628f23a7
Vinicius Costa <vini.tvf@hotmail.com>
2023-05-22 15:51:06 -0300

    Segundo commit

--- src/main/java/br/com/trier/aula1/OlaMundo.java
+++ src/main/java/br/com/trier/aula1/OlaMundo.java
@@ -2,8 +2,7 @@ package br.com.trier.aula1;
 
 public class OlaMundo {
 	public static void main(String[] args) {
-		
-		
-		
+		System.out.println("Olá Mund!");
+		System.out.println("*********");
 	}
 }
